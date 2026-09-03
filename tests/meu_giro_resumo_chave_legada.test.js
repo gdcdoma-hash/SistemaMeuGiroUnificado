b68b775ce7ee3fd98b9634bfd7dff7f15ba7850d
gdcdoma-hash <gdcdoma@gmail.com>
2026-09-03 07:10:51 -0300

    Update legacy summary key test for locked implementation

--- tests/meu_giro_resumo_chave_legada.test.js
+++ tests/meu_giro_resumo_chave_legada.test.js
@@ -5,11 +5,11 @@ const test = require('node:test');
 
 const utils = fs.readFileSync(path.resolve(__dirname, '..', 'Meu Giro', 'Utils.gs'), 'utf8');
 const inicio = utils.indexOf('function meuGiroResumoBuildChave_(idDgmb, idDesafio, idItemEstoque, metaKm, idInscricao)');
-const fim = utils.indexOf('\n}\n\n\n\nfunction calcularStatusMeuGiroPorPercentual_', inicio) + 3;
+const fim = utils.indexOf('\nfunction calcularStatusMeuGiroPorPercentual_', inicio);
 assert.ok(inicio >= 0 && fim > inicio, 'meuGiroResumoBuildChave_ deve existir');
 const buildChave = utils.slice(inicio, fim);
 
-const inicioAtualizar = utils.indexOf('function atualizarMeuGiroResumo_(idDgmb, opcoes)');
+const inicioAtualizar = utils.indexOf('function atualizarMeuGiroResumoComLockAdquirido_(idDgmb, opcoes)');
 const fimAtualizar = utils.indexOf('\nfunction atualizarMeuGiroResumoEmLote_', inicioAtualizar);
 assert.ok(inicioAtualizar >= 0 && fimAtualizar > inicioAtualizar, 'atualizarMeuGiroResumo_ deve existir');
 const atualizarResumo = utils.slice(inicioAtualizar, fimAtualizar);
